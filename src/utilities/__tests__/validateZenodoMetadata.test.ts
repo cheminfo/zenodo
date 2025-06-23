c833cffde7703ba5f96b0913e7b02761e1db6441
Eiafuawn <kenan.henzelin@proton.me>
2025-06-23 15:12:50 +0200

    test: added invalid metadata test

--- src/utilities/__tests__/validateZenodoMetadata.test.ts
+++ src/utilities/__tests__/validateZenodoMetadata.test.ts
@@ -32,3 +32,20 @@ test('ZenodoMetadata as type (typed using json-schema-to-ts)', () => {
 
   expect(() => validateZenodoMetadata(metadata)).not.toThrow();
 });
+
+test('ZenodoMetadata with additional field', () => {
+  const invalidMetadata = {
+    upload_type: 'dataset',
+    publication_date: '2025-06-16',
+    title: 'test dataset from npm library zenodo',
+    creators: [{ name: 'test' }],
+    description: 'test',
+    access_right: 'open',
+    license: 'CC0-1.0',
+    publisher: 'Zenodo', // should be 'imprint_publisher'
+  };
+
+  expect(() => validateZenodoMetadata(invalidMetadata)).toThrow(
+    'must NOT have additional properties',
+  );
+});
