518849cd1da438ba7b94ef9980e81bbf812ca5a7
Eiafuawn <kenan.henzelin@proton.me>
2025-06-27 15:52:54 +0200

    fix: zip.js web compatibility

--- src/utilities/zipFiles.ts
+++ src/utilities/zipFiles.ts
@@ -1,4 +1,9 @@
-import { ZipWriter } from '@zip.js/zip.js';
+import { ZipWriter, configure } from '@zip.js/zip.js';
+
+configure({
+  useCompressionStream: true,
+  useWebWorkers: false,
+});
 
 /**
  *
@@ -10,22 +15,20 @@ import { ZipWriter } from '@zip.js/zip.js';
 export async function zipFiles(files: File[], zipName: string): Promise<File> {
   const zipFileStream = new TransformStream();
   const zipFileBlobPromise = new Response(zipFileStream.readable).blob();
-
   const zipWriter = new ZipWriter(zipFileStream.writable);
 
-  const attachmentPromises = files.map(async (file) => {
-    const blob = new Blob([await file.arrayBuffer()]).stream();
-    await zipWriter.add(file.name, blob);
-  });
+  const attachmentPromises = files.map((file) =>
+    zipWriter.add(file.name, file.stream()),
+  );
+
   await Promise.all(attachmentPromises);
   await zipWriter.close();
 
-  const zipFile = await zipFileBlobPromise;
-  if (zipFile.size > 50 * 1024 * 1024 * 1024) {
-    throw new Error(`Zip file exceeds Zenodo's 50GB limit.`);
-  }
-
   const zipFileBlob = await zipFileBlobPromise;
+
+  if (zipFileBlob.size > 50 * 1024 * 1024 * 1024) {
+    throw new Error(`Zip file exceeds 50GB limit`);
+  }
 
   return new File([zipFileBlob], `${zipName}.zip`, {
     type: 'application/zip',
